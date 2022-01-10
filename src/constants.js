102beb32d494c1983a4657b887119d83fea8c606
Paiareli <sashapayareli@gmail.com>
2022-01-10 12:30:16 +0200

    module2-task3 server command added

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,13 @@ module.exports = {
     success: 0,
     error: 1,
   },
+  HttpCode: {
+    OK: 200,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+    FORBIDDEN: 403,
+    UNAUTHORIZED: 401,
+  },
   DEFAULT_COMMAND: `--help`,
   USER_ARGV_INDEX: 2,
 };
